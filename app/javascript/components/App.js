1cd8e569271ab6dacedad0dd6a520a2bf8c512db
Grady Shelton <gradyish@gmail.com>
2019-09-21 16:43:54 -0700

    refactor forminput into class component

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -2,34 +2,9 @@ import React, { Component } from 'react';
 import FormInputs from './FormInputs';
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      name: '',
-      address: '',
-      income: 0,
-      requestedAmount: 0
-    }
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
-
-  handleChange(event) {
-    this.setState({ 
-      name: event.target.name,
-      address: event.target.value,
-      income: event.target.value,
-      requestedAmount: event.target.value
-    });
-  }
-
-  handleSubmit(event) {
-    alert("A loan application was submitted: " + this.state);
-  }
-
   render() {
     return(
-      <FormInputs handleChange={this.handleChange}/>
+      <FormInputs/>
     )
   }
 }
--- app/javascript/components/FormInputs.js
+++ app/javascript/components/FormInputs.js
@@ -1,37 +1,61 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const FormInputs = (props) => {
-  return (
-    <div>
-      <h3>Form Inputs</h3>
-      <form>
-        <div>
-          <label>
-            Name:
+class FormInputs extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: '',
+      address: '',
+      income: 0,
+      requestedAmount: 0
+    }
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleSubmit = async event => {
+    event.preventDefault();
+    this.setState({
+      name: event.target.name,
+      address: event.target.value,
+      income: event.target.value,
+      requestedAmount: event.target.value
+    });
+    alert("A loan application was submitted: " + this.state);
+    console.log(this.state);
+  }
+  render() {
+    return (
+      <div>
+        <h3>Form Inputs</h3>
+        <form onSubmit={this.handleSubmit}>
+          <div>
+            <label>
+              Name:
           </label>
-          <input type="text" name="name" />
-        </div>
-        <div>
-          <label>
-            Mailing Address:
+            <input type="text" name="name" />
+          </div>
+          <div>
+            <label>
+              Mailing Address:
           </label>
-          <input type="text" name="address" />
-        </div>
-        <div>
-          <label>
-            Annual Income:
+            <input type="text" name="address" />
+          </div>
+          <div>
+            <label>
+              Annual Income:
           </label>
-          <input type="number" name="income" />
-        </div>
-        <div>
-          <label>
-            Requested Loan Amount:
+            <input type="number" name="income" />
+          </div>
+          <div>
+            <label>
+              Requested Loan Amount:
           </label>
-          <input type="number" name="requestedAmount" />
-        </div>
-        <input type="submit" value="Submit"/>
-      </form>
-    </div>
-  )
+            <input type="number" name="requestedAmount" />
+          </div>
+          <input type="submit" value="Submit" />
+        </form>
+      </div>
+    )
+  }
 }
 export default FormInputs;
